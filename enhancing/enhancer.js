cd7175a88c138c20ef87d73d39e1462318b2664a
Paulo Alexandre <furtuna.alexandre@gmail.com>
2020-10-07 09:21:43 -0500

    added all tests for success method

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -10,12 +10,16 @@ module.exports = {
 function success(item) {
 	return {
 		...item,
-		enhancement: item.enhancement >= 20 ? item.enhancement : item.enhancement++,
+		enhancement: item.enhancement >= 20 ? item.enhancement : ++item.enhancement,
 	};
 }
 
 function fail(item) {
-	return { ...item };
+	return {
+		...item,
+		durability: item.enhancement < 15 ? (item.durability -= 5) : (item.durability -= 10),
+		enhancement: item.enhancement >= 16 ? item.enhancement-- : item.enhancement,
+	};
 }
 
 function repair(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,21 @@
+const { expect } = require('@jest/globals');
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('Success Method', () => {
+	it('Increases by 1', () => {
+		const expectedItem = { name: 'car', durability: 100, enhancement: 12 };
+		const item = enhancer.success({ name: 'car', durability: 100, enhancement: 11 });
+		expect(expectedItem).toMatchObject(item);
+	});
+	it("Don't increase over 20", () => {
+		const expectedItem = { name: 'car', durability: 100, enhancement: 20 };
+		const item = enhancer.success({	name: 'car', durability: 100, enhancement: 20 });
+		expect(expectedItem).toMatchObject(item);
+	});
+	it("Don't increase", () => {
+		const expectedItem = { name: 'car', durability: 100, enhancement: 21 };
+		const item = enhancer.success({ name: 'car', durability: 100, enhancement: 21 });
+		expect(expectedItem).toMatchObject(item);
+	});
+});
